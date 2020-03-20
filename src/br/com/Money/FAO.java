781a6582b3df7642947e76386674a218b803c304
bruno <bruno@DESKTOP-N5NSBDO>
2020-03-20 20:08:04 -0300

    grava arquivo, falta implementar

--- src/br/com/Money/FAO.java
+++ src/br/com/Money/FAO.java
@@ -5,56 +5,69 @@ package br.com.Money;
  * 
  */
 
-
-
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.nio.charset.Charset;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
 import java.util.ArrayList;
 
 public class FAO {
 
-	
 	public void readFile(ArrayList<Account> contas) throws FileNotFoundException, IOException {
 		Path path = Paths.get("Accounts.txt");
-		
+
 		try {
 			BufferedReader reader = Files.newBufferedReader(path, Charset.defaultCharset());
 			String line;
 			String linha[] = new String[4];
-			while((line = reader.readLine()) != null) {
+			while ((line = reader.readLine()) != null) {
 				System.out.println(line);
 				linha = line.split(",");
 				int accountNumber = Integer.parseInt(linha[0]);
 				int pin = Integer.parseInt(linha[1]);
 				double availableBalance = Double.parseDouble(linha[2]);
 				double totalBalance = Double.parseDouble(linha[3]);
-				
+
 				Account contaAux = new Account(accountNumber, pin, availableBalance, totalBalance);
 				contas.add(contaAux);
-				
+
 			}
 			reader.close();
-		}catch (FileNotFoundException x) {
+		} catch (FileNotFoundException x) {
 			System.err.format("FileNotFoundException: %s%n", x);
-		}catch (IOException ex) {
+		} catch (IOException ex) {
 			System.err.format("IOException: %s%n", ex);
 		}
-		
-		System.out.printf( "%-17s%-10s%-11s%12s\n", "Conta", "PIN", "Saldo", "Saldo Total" );
-		 for (Account conta : contas) {
-		 conta.toString();
-		 }
+
+		System.out.printf("%-17s%-10s%-11s%12s\n", "Conta", "PIN", "Saldo", "Saldo Total");
+		for (Account conta : contas) {
+			conta.toString();
+		}
 
 	}
-	public void writeFile(ArrayList<Account> contas) {
+
+	public void writeFile(ArrayList<Account> contas) throws FileNotFoundException, IOException {
 		Path path = Paths.get("Accounts.txt");
 
+		try {
+			BufferedReader reader = Files.newBufferedReader(path, Charset.defaultCharset());
+			BufferedWriter writer = Files.newBufferedWriter(path, Charset.defaultCharset(), StandardOpenOption.WRITE);
+			String line;
+			while ((line = reader.readLine()) != null) {
+				writer.write(line);
+			}
+			reader.close();
+			writer.close();
+		} catch (FileNotFoundException x) {
+			System.err.format("FileNotFoundException: %s%n", x);
+		} catch (IOException ex) {
+			System.err.format("IOException: %s%n", ex);
+		}
 	}
 
-	
 }
